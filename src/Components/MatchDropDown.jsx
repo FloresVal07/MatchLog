4f75529da3d864b253f3850e861271cf511098fd
FloresVal07 <floresvalentin300@gmail.com>
2025-04-27 15:56:27 -0700

    Finalized the Format of each match drop down

--- src/Components/MatchDropDown.jsx
+++ src/Components/MatchDropDown.jsx
@@ -41,17 +41,22 @@ const MatchDropdown = ({matches, headerTitle, headerImageSrc, headerImageAlt}) =
         </div>
       <div className={`dropdown-list ${isOpen ? "open" : ""}`}>
         {matches.map((match) => (
-          <div
-            key={match.id}
-            className="dropdown-item"
-            onClick={() => handleSelect(match)}
-          >
-            <h3>{match.home}</h3>
-            <img src={match.homeSrc}/>
-            <h2>{match.score}</h2>
-            <img src={match.awaySrc}/>
-            <h3>{match.away}</h3>
+          <div className="dropdown-item" key={match.id} onClick={() => handleSelect(match)}>
+          <div className="team">
+              <img src={match.homeSrc} alt={`${match.home} logo`} />
+              <h3>{match.home}</h3>
           </div>
+          
+          <div className="score">
+              <h2>{match.score}</h2>
+          </div>
+      
+          <div className="team right">
+              <img src={match.awaySrc} alt={`${match.away} logo`} />
+              <h3>{match.away}</h3>
+          </div>
+      </div>
+      
         ))}
       </div>
     </div>
