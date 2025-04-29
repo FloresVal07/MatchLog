ec727ba042b1596160f966f8769c3ee8e8a443e4
FloresVal07 <floresvalentin300@gmail.com>
2025-04-28 17:19:07 -0700

    struggling trying to work out the kinks on the match item drop down so that its fully responsive

--- src/Components/MatchDropDown.jsx
+++ src/Components/MatchDropDown.jsx
@@ -42,22 +42,18 @@ const MatchDropdown = ({matches, headerTitle, headerImageSrc, headerImageAlt}) =
       <div className={`dropdown-list ${isOpen ? "open" : ""}`}>
         {matches.map((match) => (
           <div className="dropdown-item" key={match.id} onClick={() => handleSelect(match)}>
-          <div className="team">
+            <div className="team">
               <img src={match.homeSrc} alt={`${match.home} logo`} />
-              <h3>{match.home}</h3>
-          </div>
-          
-          <div className="score">
-              <h2>{match.score}</h2>
-          </div>
-      
-          <div className="team right">
+              <div className="team-name"><h3>{match.home}</h3></div>
+            </div>
+            <div className="score">
+                <h2>{match.score}</h2>
+            </div>
+            <div className="team right">
               <img src={match.awaySrc} alt={`${match.away} logo`} />
-              <h3>{match.away}</h3>
-          </div>
-      </div>
-      
-        ))}
+              <div className="team-name"><h3>{match.away}</h3></div>
+            </div>
+          </div>))}
       </div>
     </div>
   );
