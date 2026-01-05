ac48c78261dc8851a2cba4aee5115feb9650a190
FloresVal07 <floresvalentin300@gmail.com>
2026-01-05 02:55:32 -0700

    Incorporated pulling from the teamOfTheWeek database, just needs proper populating and error handling but its looking good

--- src/Pages/LeaguePage.jsx
+++ src/Pages/LeaguePage.jsx
@@ -5,16 +5,27 @@ import { useState, useEffect } from 'react';
 import { convertToFileSyntax } from "../Components/utils.js";
 import axios from 'axios';
 
-const LEAGUE_STATS = [
-  { title: "Top Scorers", icon: "⚽", data: [["Robert Lewandowski", 15], ["Jude Bellingham", 12], ["Borja Mayoral", 10]] },
-  { title: "Top Assists", icon: "👟", data: [["Alex Baena", 9], ["Nico Williams", 7], ["Ikay Gündogan", 6]] },
-  { title: "Top Saves", icon: "🧤", data: [["Unai Simón", 54], ["Jan Oblak", 48], ["Marc-André ter Stegen", 42]] }
-];
-
 function LeaguePage() {
     const { leagueSlug } = useParams(); 
     const [ currentLeague, setCurrentLeague ]  = useState(null);
+    const [ selectedRound, setSelectedRound ] = useState(null);
+    const [ today, setToday ] = useState(new Date())
+    const [ matches, setMatches ] = useState([]);
+    const [ currentTOTW, setCurrentTOTW] = useState([]);
     const [ loading, setLoading ] = useState(false);
+
+    const handleRoundDown = () => {
+        if(selectedRound > 1){
+            setSelectedRound(selectedRound-1);
+        }
+    }
+    const handleRoundUp = () => {
+        if(selectedRound < currentLeague.numberOfRounds){
+            setSelectedRound(selectedRound+1);
+        }
+    }
+
+
 
     useEffect(() => {
         console.log(`Fetching ${leagueSlug}`);
@@ -24,16 +35,55 @@ function LeaguePage() {
               `http://localhost:4000/leagues/${leagueSlug}`
             );
             setCurrentLeague(leagueRes.data);
-            setLoading(false);
+            if (leagueRes.data.currentRound) {
+                setSelectedRound(leagueRes.data.currentRound);
+            }
           } catch (err) {
             console.error("Error fetching data: within LeaguePage.jsx", err);
           }
         };
         fetchData();
-      }, []);
+    }, []);
+
+    useEffect(() => {
+        const fetchMatches = async () => {
+            if(!currentLeague){console.log("current league is empty"); return;}
+            if(!selectedRound){console.log("current round is empty"); return;}
+            try {
+                const matchesRes = await axios.get(
+                `http://localhost:4000/matches/round/${(currentLeague._id).toString()}/${selectedRound}`
+                );
+                setMatches(matchesRes.data);
+            } catch (err) {
+                console.error("Error fetching data: within LeaguePage.jsx", err);
+            }
+        };
+        fetchMatches();
+    }, [selectedRound, currentLeague?._id]);
+
+    useEffect(() => {
+        console.log("Checking dependencies:", { 
+            leagueId: currentLeague?._id, 
+            round: selectedRound 
+        });
+        const fetchTOTW = async () => {
+            if(!currentLeague){console.log("current league is empty"); return;}
+            if(!selectedRound){console.log("current round is empty"); return;}
+            try {
+                const totwRes = await axios.get(
+                `http://localhost:4000/totw/round/${(currentLeague._id).toString()}/10`
+                );
+                setCurrentTOTW(totwRes.data);
+                setLoading(false);
+            } catch (err) {
+                console.error("Error fetching data: within LeaguePage.jsx", err);
+            }
+        };
+        fetchTOTW();
+    }, [selectedRound, currentLeague?._id])
 
     return (
-        (loading || !currentLeague) ? (
+        (loading || !currentLeague || !today) ? (
             <LoadingPage text="Loading League"/>
         ) : (
             <div className="league-page-wrapper">
@@ -92,33 +142,45 @@ function LeaguePage() {
                         */}
                         <div className="stat-cards-container">
                             {Object.entries(currentLeague.stats).map(([key, players], idx) => {
-                                // 1. Map the technical keys to human-readable titles and icons
                                 const config = {
-                                    topScorers: { title: "Top Scorers", icon: "⚽", field: "goals"},
+                                    topScorers: { title: "Top Scorers", icon: "⚽", field: "goals" },
                                     topAssisters: { title: "Top Assists", icon: "👟", field: "assists" },
                                     topSavers: { title: "Top Saves", icon: "🧤", field: "saves" }
                                 };
 
                                 const current = config[key];
-                                if (!current) return null; // Safety check
+                                if (!current) return null;
 
                                 return (
                                     <div className="card stat-card" key={key}>
                                         <div className="stat-card-title-holder">
-                                            <img src={`/assets/navBarIcons/${current.field}.png`} className="card-title image"/>
+                                            <img 
+                                                src={`/assets/navBarIcons/${current.field}.png`} 
+                                                className="card-title image" 
+                                                alt={current.title}
+                                            />
                                             <h3 className="card-title small">
                                                 {current.title}
                                             </h3>
                                         </div>
                                         <div className="stat-list">
                                             {players.map((entry, i) => (
+                                                /* The stat-row will now use justify-content: space-between in CSS */
                                                 <div className="stat-row" key={entry._id || i}>
                                                     <div className="stat-player-info">
                                                         <span className="stat-rank">{i + 1}</span>
+                                                        <img 
+                                                            src={`/assets/playerIcons/${convertToFileSyntax(entry.player?.name)}.png`} 
+                                                            className="stat-picture"
+                                                            alt=""
+                                                        />
                                                         <span className="stat-name">{entry.player?.name}</span>
                                                     </div>
-                                                    {/* Use entry[current.field] to dynamically get .goals, .assists, or .saves */}
-                                                    <div className="stat-player-value-container"><span className="stat-value">{entry[current.field]}</span></div>
+                                                    
+                                                    {/* This container will now be pushed to the max right */}
+                                                    <div className="stat-player-value-container">
+                                                        <span className="stat-value">{entry[current.field]}</span>
+                                                    </div>
                                                 </div>
                                             ))}
                                         </div>
@@ -129,26 +191,79 @@ function LeaguePage() {
                     </section>
 
                     <aside className="league-side-column">
-                    {/* Pitch Section */}
+                    {/* Pitch Section WILL PULL FROM A TEAM OF THE WEEK DATABASE STORE*/}
                     <div className="card">
                         <h3 className="card-title">Team of the Week</h3>
-                        <div className="pitch-container">
-                        <div className="pitch-line center-circle"></div>
-                        <div className="pitch-line penalty-area"></div>
-                        <div className="pitch-player p-fw" style={{top: '15%', left: '50%'}}><span>9.0</span></div>
-                        <div className="pitch-player p-mf" style={{top: '45%', left: '25%'}}><span>8.5</span></div>
-                        <div className="pitch-player p-mf" style={{top: '45%', left: '75%'}}><span>8.8</span></div>
-                        <div className="pitch-player p-gk" style={{bottom: '10%', left: '50%'}}><span>7.9</span></div>
-                        </div>
+                        {currentTOTW.length === 0 ? (
+                            <LoadingPage />
+                        ) : (
+                            <div className="pitch-container">
+                                <div className="pitch-line center-circle"></div>
+                                <div className="pitch-line penalty-area"></div>
+                                <div className="pitch-player p-fw" style={{top: '5%', left: '42%'}}><img src={`/assets/playerIcons/${convertToFileSyntax(currentTOTW[0].squad[10].player.name)}.png`}/></div>
+                                <div className="pitch-player p-fw" style={{top: '25%', left: '10%'}}><img src={`/assets/playerIcons/${convertToFileSyntax(currentTOTW[0].squad[9].player.name)}.png`}/></div>
+                                <div className="pitch-player p-fw" style={{top: '25%', left: '42%'}}><img src={`/assets/playerIcons/${convertToFileSyntax(currentTOTW[0].squad[8].player.name)}.png`}/></div>
+                                <div className="pitch-player p-fw" style={{top: '25%', left: '75%'}}><img src={`/assets/playerIcons/${convertToFileSyntax(currentTOTW[0].squad[7].player.name)}.png`}/></div>
+                                <div className="pitch-player p-mf" style={{top: '45%', left: '23%'}}><img src={`/assets/playerIcons/${convertToFileSyntax(currentTOTW[0].squad[6].player.name)}.png`}/></div>
+                                <div className="pitch-player p-mf" style={{top: '45%', left: '63%'}}><img src={`/assets/playerIcons/${convertToFileSyntax(currentTOTW[0].squad[5].player.name)}.png`}/></div>
+                                <div className="pitch-player p-df" style={{top: '65%', left: '5%'}}><img src={`/assets/playerIcons/${convertToFileSyntax(currentTOTW[0].squad[4].player.name)}.png`}/></div>
+                                <div className="pitch-player p-df" style={{top: '65%', left: '30%'}}><img src={`/assets/playerIcons/${convertToFileSyntax(currentTOTW[0].squad[3].player.name)}.png`}/></div>
+                                <div className="pitch-player p-df" style={{top: '65%', left: '55%'}}><img src={`/assets/playerIcons/${convertToFileSyntax(currentTOTW[0].squad[2].player.name)}.png`}/></div>
+                                <div className="pitch-player p-df" style={{top: '65%', left: '80%'}}><img src={`/assets/playerIcons/${convertToFileSyntax(currentTOTW[0].squad[1].player.name)}.png`}/></div>
+                                <div className="pitch-player p-gk" style={{top: '85%', left: '42%'}}><img src={`/assets/playerIcons/${convertToFileSyntax(currentTOTW[0].squad[0].player.name)}.png`}/></div>
+                            </div>
+                        )}
                     </div>
 
                     {/* Fixtures Section */}
                     <div className="card">
-                        <h3 className="card-title">Next Fixture</h3>
-                        <div className="fixture-item">
-                        <span>Rayo Vallecano</span>
-                        <div className="fixture-badge">21:00</div>
-                        <span>Getafe</span>
+                        <div className = "card-round-nav-container">
+                            <button 
+                                onClick={handleRoundDown} 
+                                disabled={selectedRound <= 1}
+                                className="card-round-nav-button"
+                            >
+                                ◀
+                            </button>
+                            <select 
+                                value={selectedRound || ""} 
+                                onChange={(e) => setSelectedRound(parseInt(e.target.value, 10))}
+                                className="card-round-nav-select"
+                            >
+                                {Array.from({ length: currentLeague.numberOfRounds }).map((_, i) => (
+                                    <option key={i} value={i + 1}>
+                                        Round {i + 1}
+                                    </option>
+                                ))}
+                            </select>
+                            <button 
+                                onClick={handleRoundUp} 
+                                disabled={selectedRound >= currentLeague.numberOfRounds}
+                                className="card-round-nav-button"
+                            >
+                                ▶
+                            </button>
+                        </div>
+                        <div className="fixture-item-container">
+                            {matches.map((match, index) => (
+                                (match?.date < today.toISOString()) ? (
+                                    <div key={index} className="fixture-item">
+                                        <img src={`/assets/teamIcons/${convertToFileSyntax(match.homeClub.name)}.png`} className="fixture-item-image"/>
+                                        <span className="fixture-item-leftside-text">{match.homeClub.name}</span>
+                                        <div className="fixture-badge">{match.score.home} : {match.score.away}</div>
+                                        <span className="fixture-item-rightside">{match.awayClub.name}</span>
+                                        <img src={`/assets/teamIcons/${convertToFileSyntax(match.awayClub.name)}.png`} className="fixture-item-image"/>
+                                    </div>
+                                ) : (
+                                    <div key={index} className="fixture-item">
+                                        <img src={`/assets/teamIcons/${convertToFileSyntax(match.homeClub.name)}.png`} className="fixture-item-image"/>
+                                        <span className="fixture-item-leftside-text">{match.homeClub.name}</span>
+                                        <div className="fixture-badge">VS</div>
+                                        <span className="fixture-item-rightside">{match.awayClub.name}</span>
+                                        <img src={`/assets/teamIcons/${convertToFileSyntax(match.awayClub.name)}.png`} className="fixture-item-image"/>
+                                    </div>
+                                )
+                            ))}
                         </div>
                     </div>
                     </aside>
